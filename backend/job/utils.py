bce5475ab7dba293d79ade5a873622e263599e98
Taruj Goyal <taruj95@gmail.com>
2023-04-15 14:33:41 -0700

    updated the code

--- backend/job/utils.py
+++ backend/job/utils.py
@@ -1,17 +1,20 @@
 import json
-from urllib.parse import urlparse
+from typing import Optional
+from urllib.parse import urlparse, quote
 
 import requests
 from pytube import YouTube
 import whisper
-import pandas as pd
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-from urllib.parse import urlencode, quote
+BERRY_CREATE_TEMPLATE_URL = "https://api.berri.ai/create_template"
+BERRY_CREATE_APP_URL = "https://api.berri.ai/create_app"
+BERRY_QUERY_APP_URL = "https://api.berri.ai/query"
+MAPPING_FILE_PATH = "./job/url_app_mapping.json"
+USER_EMAIL = "test@berri.ai"
 
-
-def download_audio_from_youtube(video_url, filename):
+def download_audio_from_youtube(video_url: str, filename: str) -> Optional[str]:
     """
     Parameters
         video_url : str : The URL of the YouTube video from which to download the audio.
@@ -24,19 +27,15 @@ def download_audio_from_youtube(video_url, filename):
     >>> download_audio_from_youtube("https://www.youtube.com/watch?v=oHWuv1Aqrzk", "audio.mp4")
     """
     try:
-        # Create a YouTube object using the provided video URL
         yt = YouTube(video_url)
-        # Filter the streams to get the first audio-only stream
         audio_stream = yt.streams.filter(only_audio=True).first()
-        # Download the audio stream and save it with the specified filename
         output_path = audio_stream.download(filename=filename)
         return output_path
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
 
-
-def load_whisper_model(model_name="tiny"):
+def load_whisper_model(model_name: str = "tiny"):
     """
     Load a Whisper model with the given model name.
 
@@ -49,8 +48,7 @@ def load_whisper_model(model_name="tiny"):
     model = whisper.load_model(model_name)
     return model
 
-
-def transcribe_audio(whisper_model, audio_file):
+def transcribe_audio(whisper_model, audio_file: str) -> str:
     """
     Transcribe an audio file using the given Whisper model.
 
@@ -64,101 +62,59 @@ def transcribe_audio(whisper_model, audio_file):
     transcription = whisper_model.transcribe(audio_file)
     return transcription['text']
 
-BERRY_CREATE_TEMPLATE_URL = "https://api.berri.ai/create_template"
-BERRY_CREATE_APP_URL = "https://api.berri.ai/create_app"
-BERRY_QUERY_APP_URL = "https://api.berri.ai/query"
-
 def create_berry_app(file_name: str) -> str:
-    """
-    Creates berry template and app
-    :param file_name: name of file in same directory
-    :returns: berry app api endpoint
-    """
-    # Creating a template, update below config later
     app_config = {
         "advanced": {
             "intent": "qa_doc",
             "search": "default"
         }
     }
-
     create_template_data = {"app_config": json.dumps(app_config)}
-
     response = requests.post(BERRY_CREATE_TEMPLATE_URL, data=create_template_data)
     template_id = response.json()["template_id"]
-    print("template_id: {}".format(template_id))
 
-    # Now let's create an app
-    create_app_data = {"template_id": template_id, "user_email": "test@berri.ai"}
-
-    print("FILE_NAME: {}".format(file_name))
-    files = {'data_source': open(file_name, 'rb')}
-
-    response = requests.post(BERRY_CREATE_APP_URL, files=files, data=create_app_data)
-    
-    print(response.text)
+    create_app_data = {"template_id": template_id, "user_email": USER_EMAIL}
+    with open(file_name, 'rb') as data_source:
+        files = {'data_source': data_source}
+        response = requests.post(BERRY_CREATE_APP_URL, files=files, data=create_app_data)
 
     api_endpoint = response.json()["api_endpoint"]
-
     return api_endpoint
 
-
 def does_url_app_exists(url: str) -> bool:
-    """
-    Method to check if we already have an app for a requested URL
-    :param url: input url
-    :returns: corresponding bool
-    """
-    mapping_file = open("./job/url_app_mapping.json", 'r')
-    mapping = json.load(mapping_file)
-
-    if url in mapping:
-        return True
-
-    return False
-
+    try:
+        with open(MAPPING_FILE_PATH, 'r') as mapping_file:
+            mapping = json.load(mapping_file)
+        return url in mapping
+    except FileNotFoundError:
+        return False
 
 def update_url_endpoint_mapping(url: str, berry_endpoint: str) -> None:
-    """
-    Update url, berry endpoint mapping
-    """
-    mapping_file_read = open("./job/url_app_mapping.json", 'r')
-    mapping = json.load(mapping_file_read)
+    try:
+        with open(MAPPING_FILE_PATH, 'r') as mapping_file:
+            mapping = json.load(mapping_file)
+    except FileNotFoundError:
+        mapping = {}
 
-    mapping_file_write = open("./job/url_app_mapping.json", 'w')
     mapping[url] = berry_endpoint
-    mapping_file_write.write(json.dumps(mapping))
 
-
+    with open(MAPPING_FILE_PATH, 'w') as mapping_file:
+        json.dump(mapping, mapping_file)
 
 def query_berry(url: str, my_query: str) -> str:
-    """
-    Query berry endpoint corresponding to an input url
-    """
-    mapping_file = open("./job/url_app_mapping.json", 'r')
-    mapping = json.load(mapping_file)
+    try:
+        with open(MAPPING_FILE_PATH, 'r') as mapping_file:
+            mapping = json.load(mapping_file)
+    except FileNotFoundError:
+        print("Mapping file not found.")
+        return ""
 
-    berry_endpoint = mapping[url]
+    berry_endpoint = mapping.get(url)
+    if not berry_endpoint:
+        print("Berry endpoint not found for the given URL.")
+        return ""
 
-    querystring = {
-        "user_email": "test@berri.ai",
-        "instance_id": urlparse(berry_endpoint).query.split('=')[-1],
-        "query": my_query,
-        "model": "gpt-3.5-turbo"
-    }
-
-    print(querystring)
-
-    # print('urlencode(my_query)=', urlencode(my_query))
-    # print('instance_id = ', urlparse(berry_endpoint).query.split('=')[-1])
-
-    # response = requests.get(url, params=querystring)
     instance_id = urlparse(berry_endpoint).query.split('=')[-1]
-    response = requests.get(f'https://api.berri.ai/query?user_email=test@berri.ai&instance_id={instance_id}&query={quote(my_query)}')
 
-    # print('query_berry response = ', response)
-
+    response = requests.get(f'https://api.berri.ai/query?user_email={USER_EMAIL}&instance_id={instance_id}&query={quote(my_query)}')
     return response.json()['response']
-
-
-
--- backend/job/views.py
+++ backend/job/views.py
@@ -1,66 +1,55 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Job
 from .serializers import JobSerializer
-from django.shortcuts import get_object_or_404
-from rest_framework import status
-from django.db.models import Avg, Min, Max, Count
 from .filters import JobsFilter
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated
-from django.views.decorators.csrf import csrf_exempt
 from .utils import *
 
-# Create your views here.
+# Load the Whisper model
 model = load_whisper_model()
 file_path_transcript = "./job/transcript_blob.txt"
 
 @api_view(['GET'])
 def getAllJobs(request):
-    filterSet = JobsFilter(request.GET, queryset=Job.objects.all().order_by('id'))
-
-    count = filterSet.qs.count()
-    # jobs = Job.objects.all()
+    filter_set = JobsFilter(request.GET, queryset=Job.objects.all().order_by('id'))
+    count = filter_set.qs.count()
 
     # Pagination
-    resPerPage = 3
+    res_per_page = 3
     paginator = PageNumberPagination()
-    paginator.page_size = resPerPage
-
-    queryset = paginator.paginate_queryset(filterSet.qs, request)
+    paginator.page_size = res_per_page
+    queryset = paginator.paginate_queryset(filter_set.qs, request)
 
     serializer = JobSerializer(queryset, many=True)
-
     return Response({
-        "resPerPage": resPerPage,
+        "resPerPage": res_per_page,
         "count": count,
         "jobs": serializer.data
     })
-
 
 @csrf_exempt
 @api_view(['POST'])
 def getTranscript(request):
     data = request.data
     url = data['url']
-    print('backend received url = ', url)
 
     if does_url_app_exists(url):
         return Response({'message': 'random'}, status=status.HTTP_200_OK)
 
     output_string = transcribe_audio(model, download_audio_from_youtube(url, "audio.mp4"))
-    #output_string = "my random string"
     with open(file_path_transcript, 'w') as file:
-      file.write(output_string)
+        file.write(output_string)
     
     berry_endpoint = create_berry_app(file_name=file_path_transcript)
-
     update_url_endpoint_mapping(url, berry_endpoint)
 
     return Response({'message': 'random'}, status=status.HTTP_200_OK)
-
 
 @csrf_exempt
 @api_view(['POST'])
@@ -70,11 +59,4 @@ def queryEndpoint(request):
     query = data['my_query']
 
     response = query_berry(url, query)
-
-    # print('queryEndpoint output = ', response)
-
-
     return Response({'message': response}, status=status.HTTP_200_OK)
-
-
-
